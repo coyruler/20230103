4079b984ea0feb5f767a4dab9f3e59a21b5ef14d
“Tony” <tunglin.yang1@gmail.com>
2023-01-03 15:29:53 +0800

    123加入專案檔案。

---
+++ WebApplication1/Controllers/AjaxController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class AjaxController : Controller
+    {
+        NorthwindContext _context = null;
+        public AjaxController(NorthwindContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public string Greet(string Name)
+        {
+            Thread.Sleep(3000);
+            return $"Hello: {Name}";
+        }
+        [HttpPost(),ActionName("Greet")]
+        public string PostGreet(string Name)
+        {
+            return $"Hello, {Name}";
+        }
+
+        [HttpPost]
+        public string CheckName(string CompanyName)
+        {
+            bool Exists = _context.Customers.Any(c => c.CompanyName == CompanyName);
+            return Exists ? "true" : "false" ;
+        }
+
+    }
+}
